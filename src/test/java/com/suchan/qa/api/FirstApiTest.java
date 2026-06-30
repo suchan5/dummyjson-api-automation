d1d7e3e03b5778768da9a36d212451a0ac8547cd
suchan5 <suchan5@hotmail.com>
2026-06-30 16:01:15 +0800

    Create BaseTest and configure common RestAssured base URI

--- src/test/java/com/suchan/qa/api/FirstApiTest.java
+++ src/test/java/com/suchan/qa/api/FirstApiTest.java
@@ -1,11 +1,20 @@
 package com.suchan.qa.api;
 
+import com.suchan.qa.base.BaseTest;
 import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
 
-public class FirstApiTest {
+public class FirstApiTest extends BaseTest {
     @Test
     void firstTest() {
         System.out.println("Hello Junit!");
+        given()
+                .log().all()
+                .when()
+                .get("/posts/1")
+                .then()
+                .log().all()
+                .statusCode(200);
 
     }
 }
